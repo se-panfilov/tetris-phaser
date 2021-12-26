ef06c78947ab2367b0d6e30383071e99d0cb2706
Sergei Panfilov <se-panfilov@ya.ru>
2021-12-26 22:04:19 +0100

    webpack

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function sum2(a, b) {
-  return a + b + 1;
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
 }
+
+document.body.appendChild(component());
