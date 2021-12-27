d8fc1b70bc2ba01c6982556b1374eac24aaf6635
Sergei Panfilov <se-panfilov@ya.ru>
2021-12-27 13:31:29 +0100

    added extract common code

--- webpack.config.js
+++ webpack.config.js
@@ -61,5 +61,10 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 };
