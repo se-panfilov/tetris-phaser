2b424225d7349b3e9b2dc7046d19b063048a54fb
Sergei Panfilov <se-panfilov@ya.ru>
2021-12-28 01:53:48 +0100

    added tutorial game

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,165 @@
-function component(): Record<string, any> {
-  const arr = { a: 1 };
-  arr.a = 2;
-  console.log(1);
-  return arr;
+import Phaser, { Scene } from 'phaser';
+import sky from '@/assets/sky.png';
+import platform from '@/assets/platform.png';
+import star from '@/assets/star.png';
+import bomb from '@/assets/bomb.png';
+import dude from '@/assets/dude.png';
+// import SpriteWithDynamicBody = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+// import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
+// import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+
+const config: any = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+
+const game = new Phaser.Game(config);
+console.log(game);
+
+function preload(this: Scene) {
+  this.load.image('sky', sky);
+  this.load.image('ground', platform);
+  this.load.image('star', star);
+  this.load.image('bomb', bomb);
+  this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
+}
+
+// let platforms: StaticGroup;
+let platforms: any;
+// let player: SpriteWithDynamicBody;
+let player: any;
+// let cursors: CursorKeys;
+let cursors: any;
+let stars: any;
+let bombs: any;
+let gameOver: boolean = false;
+let score: number = 0;
+let scoreText: any;
+
+function collectStar(player: any, star: any) {
+  star.disableBody(true, true);
+
+  //  Add and update the score
+  score += 10;
+  scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0) {
+    //  A new batch of stars to collect
+    stars.children.iterate(function (child: any) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    const x = player.x < 400 ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+    const bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+function hitBomb(this: Scene, player: any, bomb: any) {
+  console.log(bomb);
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
+}
+
+function create(this: Scene) {
+  this.add.image(400, 300, 'sky');
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child: any) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+
+  bombs = this.physics.add.group();
+
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' } as any);
+
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null as any, this);
+  this.physics.add.collider(player, bombs, hitBomb, null as any, this);
+}
+
+function update() {
+  if (gameOver) {
+    return;
+  }
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 }
