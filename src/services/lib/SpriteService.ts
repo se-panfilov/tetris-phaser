4f466883a8e5772248991f1eb240e9feb51e79f5
Sergei Panfilov <se-panfilov@ya.ru>
2022-01-19 00:37:09 +0100

    refactoring

--- src/services/lib/SpriteService.ts
+++ src/services/lib/SpriteService.ts
@@ -1,18 +1,22 @@
-import { Application, Loader, LoaderResource, Sprite } from 'pixi.js';
+import { Application, Loader, LoaderResource, Sprite, Texture } from 'pixi.js';
 import { Dict } from '@pixi/utils';
 import { isNotDefined } from '@/utils';
 import { SpriteSize } from '@/models';
 import { getApplication } from '@/globals';
 
 export function loadSprite(name: string, spriteURL: string): Promise<Sprite> {
+  return new Promise((resolve) =>
+    loadTexture(name, spriteURL).then((texture: Texture | undefined) => resolve(new Sprite(texture)))
+  );
+}
+
+export function loadTexture(name: string, spriteURL: string): Promise<Texture | undefined> {
+  // TODO (S.Panfilov) let's not recreate loader each time!
   const loader = new Loader();
   return new Promise((resolve, reject) => {
-    loader.add(name, spriteURL).load((loader: Loader, resources: Dict<LoaderResource>) => {
-      // const loader = Loader.shared.resources[spriteURL];
-      if (isNotDefined(loader) || isNotDefined(resources[name]?.texture))
-        return reject(new Error(`Cannot load resource for "${spriteURL}"`));
-      const sprite = new Sprite(resources[name]?.texture);
-      resolve(sprite);
+    return loader.add(name, spriteURL).load((loader: Loader, resources: Dict<LoaderResource>) => {
+      if (isNotDefined(resources[name]?.texture)) return reject(new Error(`Cannot load resource for "${spriteURL}"`));
+      resolve(resources[name]?.texture);
     });
   });
 }
