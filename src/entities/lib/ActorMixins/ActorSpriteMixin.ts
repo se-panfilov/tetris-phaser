4a7ca37b14849299b57e200afab4b920d82952ef
Sergei Panfilov <se-panfilov@ya.ru>
2022-11-16 20:28:55 +0100

    fixes

--- src/entities/lib/ActorMixins/ActorSpriteMixin.ts
+++ src/entities/lib/ActorMixins/ActorSpriteMixin.ts
@@ -5,35 +5,31 @@ import { isDefined, isNotDefined } from '@/utils';
 
 export function ActorSpriteMixin(name: string, { width, height, spriteURL }: ActorConfig): IActorSpriteMixin {
   const sprite$ = new BehaviorSubject<WrappedSprite | undefined>(undefined);
-  const spritePosition$ = new BehaviorSubject<ActorPosition>({ x: 0, y: 0 });
-  const spriteOrientation$ = new BehaviorSubject<number>(0);
+  const position$ = new BehaviorSubject<ActorPosition>({ x: 0, y: 0 });
+  const orientation$ = new BehaviorSubject<number>(0);
 
   LoadActorSprite(name, spriteURL, { height, width }).then((sprite) => sprite$.next(sprite));
 
-  combineLatest([sprite$, spritePosition$]).subscribe(([sprite, { x, y }]) => {
+  combineLatest([sprite$, position$, orientation$]).subscribe(([sprite, { x, y }, orientation]) => {
     if (isNotDefined(sprite)) return;
     sprite.x = x;
     sprite.y = y;
-  });
-
-  combineLatest([sprite$, spriteOrientation$]).subscribe(([sprite, orientation]) => {
-    if (isNotDefined(sprite)) return;
     sprite.rotation = orientation;
   });
 
   function destroy(): void {
     if (isDefined(sprite$?.value)) sprite$.value.destroy();
     sprite$.complete();
-    spritePosition$.complete();
-    spriteOrientation$.complete();
+    position$.complete();
+    orientation$.complete();
   }
 
-  return { destroy, sprite$, spritePosition$, spriteOrientation$ };
+  return { destroy, sprite$, position$, orientation$ };
 }
 
 export interface IActorSpriteMixin {
   readonly sprite$: BehaviorSubject<WrappedSprite | undefined>;
-  readonly spritePosition$: BehaviorSubject<ActorPosition>;
-  readonly spriteOrientation$: BehaviorSubject<number>;
+  readonly position$: BehaviorSubject<ActorPosition>;
+  readonly orientation$: BehaviorSubject<number>;
   readonly destroy: () => void;
 }
